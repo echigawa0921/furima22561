1941aa5e097dbdbee603ca1e20596b95a05d1237
echigawa0921 <yuki.echigawa@di-v.co.jp>
2020-10-24 19:58:38 +0900

    ログインページへのリダイレクト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,11 +7,17 @@ class ItemsController < ApplicationController
     end
 
     def create
+        @item = Item.new(items_params)
+        if @item.save
+            redirect_to controller: :items, action: :index
+        else
+            render "new"
+        end
     end
 
     private
 
-    def itemes_params
-        params.require(:item).permit(:name, :explanation, :categoly_id, :status_id,:shipping_charge_id, :shipping_region_id, :days_until_shipping_id,:price, :image).merge(user_id: current_user.id)
+    def items_params
+        params.require(:item).permit(:name, :explanation,:category_id, :status_id,:shipping_charge_id, :shipping_region_id, :days_until_shipping_id,:price, :image).merge(user_id: current_user.id)
     end
 end
