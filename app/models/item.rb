879c0bde343fb0803ca657dcb4d1adebffdbb612
echigawa0921 <yuki.echigawa@di-v.co.jp>
2020-10-24 17:52:42 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+    belongs_to :user
+    has_one :order
+    has_one_attached :image
+
+    private
+
+    def itemes_params
+        params.require(:item).permit(:name, :explanation, :categoly_id, :status_id,:shipping_charge_id, :shipping_region_id, :days_until_shipping_id,: price, :created_at,:update_at, :user_id).merge(user_id: current_user.id)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+  has_many :orders
+
   validates :nickname,:birthday, presence: true
   validates :first_name,:last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/} 
   validates :first_name_kana,:last_name_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/}
