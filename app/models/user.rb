10e315f4dd9e86c5dcb2a4d93a173913af4c9a79
echigawa0921 <yuki.echigawa@di-v.co.jp>
2020-11-03 22:07:08 +0900

    購入ページへのルーティング

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
 
   has_many :items
   has_many :orders
 
-  validates :nickname,:birthday, presence: true
-  validates :first_name,:last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/} 
-  validates :first_name_kana,:last_name_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/}
-  validates :password, presence: true, format:{with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         validates :nickname,:birthday, presence: true
+         validates :first_name,:last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/} 
+         validates :first_name_kana,:last_name_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/}
+         validates :password, presence: true, format:{with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items
+  resources :orders
+  resources :orders, only:[:create]
 end
