3f79c13c78fb9310a7c4f2129d3aeb411bd0306d
echigawa0921 <yuki.echigawa@di-v.co.jp>
2020-11-03 23:10:03 +0900

    購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,37 @@
 class OrdersController < ApplicationController
 
     def index
-      @order = Order.new
-    end
-  
-    def create
-      @order = Order.new(order_params)
-      if @order.valid?
-        @order.save
-        return redirect_to root_path
-      else
-        render 'index'
+        @order = Order.new
+
       end
+    
+      def create
+        @order = Order.new(order_params)
+        if @order.valid?
+          @order.save
+          return redirect_to root_path
+        else
+          render 'index'
+        end
+      end
+
+      def show
+        @item = Item.find(params[:id])
     end
-  
-    private
-  
-    def order_params
-      params.require(:order).permit(:price)
+
+    def edit
+        @item = Item.find(params[:id])
     end
-  
-  end
+    
+    def update
+        @item = Item.find(params[:id])
+        @item.update(item_params)
+    end
+    
+      private
+
+      def order_params
+
+      end
+    
+end
