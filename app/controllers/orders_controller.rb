fee2c5658ab9666cf192206b6a6586744dc0f7a7
echigawa0921 <yuki.echigawa@di-v.co.jp>
2020-11-10 21:43:40 +0900

    Update orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -21,3 +21,27 @@ class OrdersController < ApplicationController
       params.require(:user_item_order).permit(:postal_code, :prefecture_id,:city, :house_number, :builing_name, :phone_number).merge(user_id: current_user.id).merge(item_id: params[:item_id])
     end
 end
+
+class OrdersController < ApplicationController
+
+  def index
+    @order = UserItemOrder.new
+  end
+
+  def create
+    @order = UserItemOrder.new(order_params)
+
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+private
+
+def order_params
+  params.require(:user_item_order).permit(:postal_code, :prefecture_id,:city, :house_number, :builing_name, :phone_number).merge(user_id: current_user.id).merge(item_id: params[:item_id])
+end
+end
