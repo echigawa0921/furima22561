9d1e7a3e452d9e37873b691ecb93f7cb5f3f9c43
echigawa0921 <yuki.echigawa@di-v.co.jp>
2020-11-11 23:06:30 +0900

    商品購入機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
   resources :items do
     resources :orders
   end
+
+  resources :credit_cards, only: [:show, :destroy] do
+    collection do
+      post 'pay', to: 'credit_cards#pay'
+    end
+  end
+  
 end
--- db/migrate/20201111124706_create_credit_cards.rb
+++ db/migrate/20201111124706_create_credit_cards.rb
@@ -1,6 +1,12 @@
 class CreateCreditCards < ActiveRecord::Migration[6.0]
   def change
     create_table :credit_cards do |t|
+      t.integer :user_id, null: false
+      t.string :customer_id, null: false
+      t.string :card_id, null: false
+      t.string :mounth, null: false
+      t.string :year, null: false
+      t.string :cvc, null: false
 
       t.timestamps
     end
