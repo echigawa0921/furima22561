92c9ae56be9535a76bcc17479f2799a4d9ad5150
echigawa0921 <yuki.echigawa@di-v.co.jp>
2020-11-13 22:51:14 +0900

    Update orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,9 +18,6 @@ class OrdersController < ApplicationController
         end
       end
 
-      def set_item
-        @item = Item.find(params[:item_id])
-      end
 
     private
 
@@ -35,6 +32,51 @@ class OrdersController < ApplicationController
         card: item_purchase_params[:token],
         currency:'jpy'
       )
+    end
+
+    def set_item
+      @item = Item.find(params[:item_id])
+    end
+end
+
+class OrdersController < ApplicationController
+  before_action :authenticate_user!,only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
+  
+
+      def index
+        @order = UserItemOrder.new
+      end
+    
+      def create
+        @order = UserItemOrder.new(order_params)
+
+        if @order.valid?
+          @order.save
+          return redirect_to root_path
+        else
+          render 'index'
+        end
+      end
+
+
+    private
+
+    def order_params
+      params.require(:user_item_order).permit(:postal_code, :prefecture_id,:city, :house_number, :builing_name, :phone_number).merge(user_id: current_user.id).merge(item_id: params[:item_id])
+    end
+
+    def pay_item
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: item_purchase_params[:token],
+        currency:'jpy'
+      )
+    end
+
+    def set_item
+      @item = Item.find(params[:item_id])
     end
 end
 
