cc93bb1325cc852edef6224757fd419aba72ae6c
echigawa0921 <yuki.echigawa@di-v.co.jp>
2020-11-16 22:34:29 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,9 +38,9 @@ class ItemsController < ApplicationController
 
     def destroy
         if @item.destroy
-          redirect_to root_path
-        else
-          render :show
+            redirect_to root_path
+          else
+            render :show
         end
     end
 
