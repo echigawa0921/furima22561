f069c5ae547d4ed99fa84550aae3ab003e8c81a8
echigawa0921 <yuki.echigawa@di-v.co.jp>
2020-11-18 22:03:28 +0900

    Update 20201117012655_create_categories.rb

--- db/migrate/20201117012655_create_categories.rb
+++ db/migrate/20201117012655_create_categories.rb
@@ -6,3 +6,11 @@ class CreateCategories < ActiveRecord::Migration[6.0]
     end
   end
 end
+class CreateCategories < ActiveRecord::Migration[6.0]
+  def change
+    create_table :categories do |t|
+
+      t.timestamps
+    end
+  end
+end
