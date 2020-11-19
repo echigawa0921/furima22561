f92663090be8d141624ddc231413fd106361c1e2
echigawa0921 <yuki.echigawa@di-v.co.jp>
2020-11-20 07:12:37 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -12,18 +12,5 @@ Rails.application.routes.draw do
   end
   
 end
-Rails.application.routes.draw do
-  devise_for :users
-  root to: 'items#index'
-  resources :items do
-    resources :orders
-  end
 
-  resources :credit_cards, only: [:show, :destroy] do
-    collection do
-      post 'pay', to: 'credit_cards#pay'
-    end
-  end
-  
-end
 
